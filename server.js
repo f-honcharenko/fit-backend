22267555876647f3a0c59a03edf4921fe45c564d
Filipp Honcharenko <akstoplay@gmail.com>
2020-10-15 20:26:44 +0300

    remove unnecessary HTTP API`s and packages

--- server.js
+++ server.js
@@ -14,75 +14,16 @@ app.use(bodyPaster.urlencoded({ extended: false }));
 
 //routes
 
-app.put('/options/group/addnew', (req, res, next) => {
-    console.log(req.body);
-    fs.readFile('./data/options.json', 'utf8', function(err, contents) {
-        if (err) {
-            return console.log(err);
-        }
-        let file = JSON.parse(contents);
-        let pattern = {
-            id: "id",
-            users: "user",
-            name: "ПП11/1",
-            spec: "ПП1"
-        };
-        let name = req.body.groupName;
-        let spec = req.body.groupSpec;
-        file.groups[spec+name] = {};
-        file.groups[spec+name] = pattern;
-        console.log('1',file.groups);
-        file.groups[spec+name].name = spec+name;
-        file.groups[spec+name].spec = spec;
-        file.groups[spec+name].id = +file.groups.length+1;
-        console.log('FILE',file);
-        fs.writeFileSync('./data/options.json', JSON.stringify(file, null, 2), function (err, contents) { 
-            console.log(err)
-            console.log(contents)
-        });
-        // res.json(JSON.parse(contents));
-    });
-});
-
-app.get('/options/get', (req, res, next) => {
-    console.log(req.body);
-    fs.readFile('./data/options.json', 'utf8', function(err, contents) {
-        if (err) {
-            return console.log(err);
-        }
-        res.json(JSON.parse(contents));
-    });
-});
-
-app.put('/options/teachers/addnew', (req, res, next) => {
-    console.log(req.body);
-    fs.readFile('./data/options.json', 'utf8', function(err, contents) {
-        if (err) {
-            return console.log(err);
-        }
-        let teacherFIO = req.body.FIO;
-        let file = JSON.parse(contents);
-        console.log(file.teachers);
-        file.teachers.push(teacherFIO);
-        console.log(file.teachers);
-        fs.writeFileSync('./data/options.json', JSON.stringify(file, null, 2), function (err, contents) { 
-            if (err) return err
-            console.log(contents)
-        });
-
-    });
-});
-
 app.post('/timetables/1/add', (req, res, next) => {
     let data = req.body;
-    console.log(data);
-    fs.readFile('./data/ИПЗ-3/timetable.json', 'utf8', function(err, contents) {
+    const spec = req.body.spec;
+    //read the timуеtable
+    fs.readFile('./data/'+spec+'/timetable.json', 'utf8', function(err, contents) {
         if (err) {
-            return console.log(err);
+            return res.status(500).json(err);
         }
         let file = JSON.parse(contents);
-        // console.log(file);
-        // let dates 
+        //pasrsing
         data.lessonDate.forEach(element => {
             console.log(element);
             if (file[element]) {
@@ -113,8 +54,6 @@ app.post('/timetables/1/add', (req, res, next) => {
                             file[element][group][data.lessonTime].color = data.lessonColor;      
                             file[element][group][data.lessonTime].link = data.lessonLink; 
                     }
-                    
-                    
                 });
             } else {
                 file[element] = {};
@@ -127,28 +66,14 @@ app.post('/timetables/1/add', (req, res, next) => {
                     file[element][group][data.lessonTime].color = data.lessonColor;      
                     file[element][group][data.lessonTime].link = data.lessonLink;      
                 });
-                // file[element][data.lessonTime] = {};
             }
         });
 
-
-
-        
-        fs.writeFileSync('./data/ИПЗ-3/timetable.json', JSON.stringify(file, null, 2), function (err, contents) { 
-            if (err) return err
-            console.log(contents)
+        //save the new timetable
+        fs.writeFileSync('./data/'+spec+'/timetable.json', JSON.stringify(file, null, 2), function (err, contents) { 
+            if (err) return res.status(500).json(err);
+            res.status(200);
         });
-        res.status(200);
-    });
-});
-
-app.get('/timetables/1/get', (req, res, next) => {
-    console.log(req.body);
-    fs.readFile('./data/timetable.json', 'utf8', function(err, contents) {
-        if (err) {
-            return console.log(err);
-        }
-        res.json(JSON.parse(contents));
     });
 });
 
@@ -160,20 +85,16 @@ app.post('/timetables/1/getCal', (req, res, next) => {
     const year = new Date(new Date(dateStart).setDate(new Date(dateStart).getDate() + 15)).getFullYear();
     const dateEnd = req.body.end;
     let responce = [];
-    // console.log('sart'+dateStart);
-    // console.log('end'+dateEnd);
+    //read the timetable;
     fs.readFile('./data/'+spec+'/timetable.json', 'utf8', function(err, contents) {
         if (err) {
-            return console.log(err);
+            return res.status(500).json(err);
         }
         let file = JSON.parse(contents);
-        // console.log(file);
         for (key in file) {
-            // console.log(new Date(key).setHours(09,00));
-            // console.log( );
+            //!!!REWEITE THIS BLOCK!!!
             if ((key >= dateStart) && (key <= dateEnd)) {
                 for (number in file[key][group]) {
-                    let event;
                     let date = new Date(key);
                     date.setDate(date.getDate() + 1);
                     switch (Number(number)) { 
@@ -221,25 +142,16 @@ app.post('/timetables/1/getCal', (req, res, next) => {
                         default:
                             break;
                     }
-                    // console.log(event);
                 }
-                // console.log(key+' '+key.substr(0,11));
                 
             }
         }
-        // 
-        // 
-        let res2 = [];
+        //
+        //save the new timtable
         fs.readFile('./data/'+spec+'/birthday.json', 'utf8', function (err, contents) { 
-            if (err) {
-                return console.log(err);
-            }
+            if (err) return res.status(500).json(err);
             let file = JSON.parse(contents);
-            // responce = [];
-            
-            // console.log(file);
             for (key in file) {
-                // console.log();
                 file[key].forEach((el) => { 
                     let BDay = {
                         start: new Date(key).setFullYear(year),
@@ -248,20 +160,16 @@ app.post('/timetables/1/getCal', (req, res, next) => {
                         color: '#ff5733'
                     }
                     responce.push(BDay);
-                    // console.log(BDay);
                 })
-                
-                // console.log(BDay);
-                // console.log(res2.length);
-    }
-    // console.log(res2);
-    res.json(responce).status(200);
-        
+            }
+        res.json(responce).status(200);
         });
     });
 
 });
 
+
+//express start
 const port = process.env.PORT || 5000;
 app.listen(port, (err) => {
     if (err) return console.log(err);
@@ -269,7 +177,7 @@ app.listen(port, (err) => {
 }); 
 
 
-//ПАРСИНГ ДР
+//PARSING BITHDAY FROM XLSX
 // var workbook = new ExcelJS.Workbook();
 // workbook.xlsx.readFile('./data/birthday.xlsx')
 //     .then(function (file, error) {
